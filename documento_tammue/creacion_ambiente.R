3877df163f0c37523f6c058c68f967115413df1a
josezea <jfzeac@unal.edu.co>
2023-06-13 06:53:00 -0500

    Add files via upload

---
+++ documento_tammue/creacion_ambiente.R
@@ -0,0 +1,12 @@
+library(samplesize4surveys)
+library(dplyr)
+library(readxl)
+library(writexl)
+library(flextable)
+
+renv::install("tidyverse")
+renv::install("samplesize4surveys")
+renv::install("writexl")
+renv::install("flextable")
+
+# renv::snapshot()
